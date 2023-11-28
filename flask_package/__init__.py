bce424071c76730725459b5abfdd70331c7508c9
Ralph Corey <ralph.corey.1@gmail.com>
2023-11-28 05:34:38 -0800

    Update: added login and logout

--- flask_package/__init__.py
+++ flask_package/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from logging import DEBUG
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt 
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -13,5 +14,6 @@ app.logger.setLevel(DEBUG)
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from flask_package import routes
--- flask_package/routes.py
+++ flask_package/routes.py
@@ -3,6 +3,7 @@ from flask_package.models import User
 from flask_package import app, db, bcrypt 
 from flask_package.forms import RegistrationForm, LoginForm
 from flask_package.helpers import Chore, store_feedback
+from flask_login import login_user, logout_user, current_user 
 
 @app.route('/')
 @app.route('/welcome')
@@ -12,6 +13,8 @@ def welcome():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data)
@@ -31,8 +34,10 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'wcbookout@yahoo.com' and form.password.data == 'test':
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
             flash('Logged in')
+            login_user(user)
             return redirect(url_for('index'))
         else:
             flash('Oops, something went wrong')
@@ -41,6 +46,10 @@ def login():
         app.logger.error('Validation Error\n' + str(form.errors))
     return render_template("login.html", title="Login", form=form)
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('welcome'))
 
 @app.route('/index', methods=['GET', 'POST'])
 def index():
