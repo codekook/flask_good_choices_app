dd4a97e1791cb543be715bae8ac6bdc81449c565
Ralph Corey <ralph.corey.1@gmail.com>
2024-01-10 04:59:45 -0800

    Update: new database model and chore management

--- flask_package/models.py
+++ flask_package/models.py
@@ -6,7 +6,9 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    date = db.Column(db.Date, nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
@@ -15,3 +17,14 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Chore(db.Model):
+    __tablename__ = 'chores'
+    chore_id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True, autoincrement=True)
+    chore = db.Column(db.String(255), unique=True, nullable=False)
+    completed = db.Column(db.Boolean, nullable=False)
+    frequency = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return f"Chore('{self.chore_id}','{self.chore}', '{self.completed}', '{self.frequency}')"
--- flask_package/routes.py
+++ flask_package/routes.py
@@ -1,9 +1,10 @@
 from flask import render_template, request, redirect, url_for, flash
-from flask_package.models import User
+from flask_package.models import User, Chore
 from flask_package import app, db, bcrypt 
 from flask_package.forms import RegistrationForm, LoginForm
-from flask_package.helpers import Chore, store_feedback
+from flask_package.helpers import store_feedback
 from flask_login import login_user, logout_user, current_user 
+from datetime import date
 
 @app.route('/')
 @app.route('/welcome')
@@ -18,7 +19,8 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data)
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        today = date.today()
+        user = User(date=today, username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
         flash("Thanks for creating an account!")
@@ -57,28 +59,31 @@ def index():
         if request.method == "POST":
             if request.form.get('new_chore'):
                 newChore = request.form['new_chore']
-                new_chore = Chore(newChore)
-                status_check = new_chore.get_chore_completed()
-                app.logger.debug('New Chore: ' + newChore +
-                                ' | Status: ' + str(status_check))
+                new_chore = Chore(chore=newChore, frequency='Weekly', completed=False)
+                db.session.add(new_chore)
+                db.session.commit()
+                app.logger.debug('New Chore: ' + newChore)
 
             if request.form.get('rmove_chore'):
                 chore_number = request.form['chore_num']
-                Chore.chore_list[int(chore_number)].remove_chore()
-                current_chores = Chore.chore_list
-                # app.logger.debug('Chore Removed Number: ' + str(chore_number) + ' | Chore List: ' + current_chores)
+                chore_to_delete = db.delete(Chore).where(Chore.chore_id == chore_number)
+                db.session.execute(chore_to_delete)
+                db.session.commit()
+                app.logger.debug('Deleted Chore: ' + str(chore_to_delete))
 
             if request.form.get('reset'):
-                for i in Chore.chore_list:
+                pass
+                '''for i in Chore.chore_list:
                     i.set_chore_completed(False)
                     status_check = i.get_chore_completed()
                     chore_lst = Chore.chore_list
                     app.logger.debug(
                         'Chore: ' + i + '| Status: ' + str(status_check))
-                return render_template("index.html", chore_lst=chore_lst)
+                return render_template("index.html", chore_lst=chore_lst)'''
 
             if request.form.get('status_complete'):
-                chore_complete = request.form['chore_complete']
+                pass
+                '''chore_complete = request.form['chore_complete']
                 Chore.chore_list[int(chore_complete)].set_chore_completed(True)
                 status_check = Chore.chore_list[int(
                     chore_complete)].get_chore_completed()
@@ -86,8 +91,11 @@ def index():
                 app.logger.debug('Chore Completed Number: ' +
                                 chore_complete + ' | Status: ' + str(status_check))
                 return render_template("index.html", chore_lst=chore_lst)
-        chore_lst = Chore.chore_list
-        return render_template("index.html", chore_lst=chore_lst)
+        chore_lst = Chore.chore_list'''
+        #query the chore table and put existing chores into a list
+        chore_table_info = db.session.execute(db.select(Chore.chore_id, Chore.chore).order_by(Chore.chore_id)).fetchall()
+        print(chore_table_info)
+        return render_template("index.html", chore_lst=chore_table_info)
     else:
         return redirect(url_for('welcome')) 
 
